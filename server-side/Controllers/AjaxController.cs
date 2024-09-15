ad7931d7956377067f6a48f3573484a227e58e2d
Pedro Andrade <pedroandrade20xx@gmail.com>
2024-09-14 22:23:26 -0300

    chore(server): 'AjaxController' base class created

---
+++ server-side/Controllers/AjaxController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using TaskMonitor.Exceptions;
+
+namespace TaskMonitor.Controllers
+{
+    [ApiController]
+    [Route("/Ajax/[controller]")]
+    public class AjaxController() : ControllerBase
+    {
+        protected IActionResult Execute(Func<IActionResult> func)
+        {
+            try
+            {
+                return func();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Unexpected \"exception\" occurred when processing request");
+                return StatusCode(500);
+            }
+        }
+
+        protected async Task<IActionResult> ExecuteAsync(Func<Task<IActionResult>> func)
+        {
+            try
+            {
+                return await func();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Unexpected \"exception\" occurred when processing request");
+                return StatusCode(500);
+            }
+        }
+    }
+}
