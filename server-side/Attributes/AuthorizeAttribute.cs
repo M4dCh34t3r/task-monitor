b4b6ac7db88a3f1374452f76098444a02c3a4838
Pedro Andrade <pedroandrade20xx@gmail.com>
2024-09-15 08:55:07 -0300

    chore(server): default 'AuthorizeAttribute' added and 'JwtMiddleware' implemented

---
+++ server-side/Attributes/AuthorizeAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TaskMonitor.Exceptions;
+using TaskMonitor.Utils;
+
+namespace TaskMonitor.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class AuthorizeAttribute(string claim = "") : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            try
+            {
+                ClaimsPrincipal user = context.HttpContext.User;
+
+                AuthorizationUtil.ValidateUser(user);
+                AuthorizationUtil.ValidateClaims(user, claim);
+            }
+            catch (ContextResultException ex)
+            {
+                context.Result = ex.ToObjectResult();
+            }
+        }
+    }
+}
---
+++ server-side/Middlewares/JwtMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using TaskMonitor.Utils;
+
+namespace TaskMonitor.Middlewares
+{
+    public class JwtMiddleware(RequestDelegate next)
+    {
+        private readonly RequestDelegate _next = next;
+
+        public async Task InvokeAsync(HttpContext context, IConfiguration configuration)
+        {
+            if (!context.Response.Headers.Authorization.IsNullOrEmpty())
+                context.Response.Headers.Remove("Authorization");
+
+            if (JwtUtil.IsTokenValid(context, configuration.GetValue<string>("Secrets:Salts:JWT")!))
+            {
+                SentrySdk.ConfigureScope(scope =>
+                {
+                    scope.User = new SentryUser
+                    {
+                        Id = context.User.FindFirst(AuthorizationUtil.UserId)!.Value,
+                        Other = new Dictionary<string, string>()
+                        {
+                            { "Admin", context.User.FindFirst(AuthorizationUtil.Admin)!.Value },
+                        },
+                    };
+                });
+            }
+
+            await _next(context);
+        }
+    }
+
+    public static class JwtMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseJwtMiddleware(this IApplicationBuilder builder) =>
+            builder.UseMiddleware<JwtMiddleware>();
+    }
+}
